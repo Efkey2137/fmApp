8b24ba5ec62223d1a6782400f4bcc76267ebcc82
Efkey <filipploskonka17@gmail.com>
2025-02-21 19:57:20 +0100

    added webAnalitycs

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react"
 import Header from "./components/Header";
 import Menu from "./components/Menu";
 import RatePlayer from "./pages/RatePlayer";
@@ -10,7 +11,9 @@ function App() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
+    
     <Router>
+      <Analytics/>
       <div className="App">
         <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
