90a2882c64a720170cf8f31a6762117fa9775146
Efkey <filipploskonka17@gmail.com>
2025-02-23 16:21:38 +0100

    update mobilemenu functionality

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -6,16 +6,18 @@ interface MenuProps {
   setMenuOpen: (open: boolean) => void;
 }
 
-const Menu: React.FC<MenuProps> = ({ menuOpen }) => {
+const Menu: React.FC<MenuProps> = ({ menuOpen, setMenuOpen }) => {
+  const handleCloseMenu = () => setMenuOpen(false);
+
   return (
     <nav className={`menu ${menuOpen ? "active" : ""}`}>
       <ul>
-        <li><Link to="/" className="App-link">Home</Link></li>
-        <li><Link to="/compare" className="App-link">Compare</Link></li>
-        <li><Link to="/rate-player" className="App-link">Rate Player</Link></li>
-        <li><Link to="/track-progress" className="App-link">Track Progress</Link></li>
-        <li><Link to="/wonderkids" className="App-link">Wonderkids</Link></li>
-        <li><Link to="/save-ideas" className="App-link">Save Ideas</Link></li>
+        <li><Link to="/" className="App-link" onClick={handleCloseMenu}>Home</Link></li>
+        <li><Link to="/compare" className="App-link" onClick={handleCloseMenu}>Compare</Link></li>
+        <li><Link to="/rate-player" className="App-link" onClick={handleCloseMenu}>Rate Player</Link></li>
+        <li><Link to="/track-progress" className="App-link" onClick={handleCloseMenu}>Track Progress</Link></li>
+        <li><Link to="/wonderkids" className="App-link" onClick={handleCloseMenu}>Wonderkids</Link></li>
+        <li><Link to="/save-ideas" className="App-link" onClick={handleCloseMenu}>Save Ideas</Link></li>
       </ul>
     </nav>
   );
