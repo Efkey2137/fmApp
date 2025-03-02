071acbc7846a6685d71ad8e0bd37c42eef87b679
Efkey <filipploskonka17@gmail.com>
2025-03-02 01:44:00 +0100

    update dashboard layout and styles; add login card to dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Diff, Star, ChartLine, Lightbulb, SearchCheck  } from 'lucide-react';
+import { Diff, Star, ChartLine, Lightbulb, SearchCheck, User  } from 'lucide-react';
 import { Link } from "react-router-dom";
 import "../css/Dashboard.css";
 
@@ -10,6 +10,7 @@ const cards = [
   { id: 3, title: "Track Progress", icon: ChartLine, link: "/progress" },
   { id: 4, title: "Wonderkids", icon: Star, link: "/wonderkids" },
   { id: 5, title: "Save Ideas", icon: Lightbulb, link: "/save-ideas" },
+  { id: 6, title: "Log In", icon: User, link: "/" },
 ];
 
 
@@ -17,7 +18,7 @@ const Home: React.FC = () => {
   return (
     <div className="dashboard">
       {cards.map((card) => (
-        <Link to={card.link} className="card-link"><div key={card.id} className="p-4 border rounded-lg shadow flex flex-col items-center">
+        <Link to={card.link} className="card-link"><div key={card.id} className="card">
           <card.icon size={48} className="text-blue-500" />
           <h2 className="mt-2">{card.title}</h2>
         </div></Link>
